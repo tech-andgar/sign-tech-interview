8f0c7822611712e52db3fdc74b38f13aa6d7ca7b
Andrés García <dev@tech-andgar.me>
2025-03-10 16:26:23 -0500

    set base path for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://tech-andgar.github.io',
+	base: '/crack-interview-2/',
 	integrations: [
 		starlight({
 			title: 'My Docs',
