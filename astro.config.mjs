40bd8bdc5dae4568d3137c873c285784a7747726
Andrés García <dev@tech-andgar.me>
2025-03-11 14:25:57 -0500

    feat: restructure documentation and add Flutter content; remove outdated files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,54 +1,67 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 const port = 4300;
 const localHostUrl = `http://localhost:${port}`;
-const liveUrl = 'https://signlanguagetech.com';
+const liveUrl = "https://signlanguagetech.com";
 const isProd = import.meta.env.PROD;
 
 export default defineConfig({
-	server: {
-    port
-  },
+  server: { port },
   site: isProd ? liveUrl : localHostUrl,
-	base: isProd ? '/crack-interview': '/',
-	integrations: [
-		starlight({
-			plugins: [],
-			title: 'Crack Interview',
-			social: {
-				github: 'https://github.com/signlanguagetech/crack-interview',
-			},
-			sidebar: [
-				{
-          label: "Hello",
-          autogenerate: { directory: "hello/" },
-          collapsed: false
+  base: isProd ? "/crack-interview" : "/",
+  integrations: [
+    starlight({
+      plugins: [],
+      title: {
+        "en": "Crack Interview",
+        "es": "Crack Interview en Español",
+        "ir": "Crack Interview به زبان فارسی",
+      },
+      social: {
+        github: "https://github.com/signlanguagetech/crack-interview",
+      },
+      defaultLocale: 'root',
+      locales: {
+        root: {
+          label: 'English',
+          lang: 'en',
         },
-				{
+        "es": {
+          label: "Español",
+          locale: "es",
+        },
+        "fa-IR": {
+          label: "فارسی",
+          locale: "fa-IR",
+          direction: "rtl",
+        },
+      },
+      sidebar: [
+        {
           label: "Skills",
           autogenerate: { directory: "skills/" },
-          collapsed: false
+          collapsed: false,
         },
-				// {
-				// 	label: 'Hello',
-				// 	items: [
-				// 		// Each item here is one entry in the navigation menu.
-				// 		{ label: 'Hope', link: '/hello/example' },
-				// 	],
-				// },
-				// {
-				// 	label: 'Guides',
-				// 	items: [
-				// 		// Each item here is one entry in the navigation menu.
-				// 		{ label: 'Example Guide', link: '/guides/example/' },
-				// 	],
-				// },
-				// {
-				// 	label: 'Reference',
-				// 	autogenerate: { directory: 'reference' },
-				// },
-			],
-		}),
-	],
+        // {
+        // 	label: 'Hello',
+        // 	items: [
+        // 		// Each item here is one entry in the navigation menu.
+        // 		{ label: 'Hope', link: '/hello/example' },
+        // 	],
+        // },
+        // {
+        // 	label: 'Guides',
+        // 	items: [
+        // 		// Each item here is one entry in the navigation menu.
+        // 		{ label: 'Example Guide', link: '/guides/example/' },
+        // 	],
+        // },
+        // {
+        // 	label: 'Reference',
+        // 	autogenerate: { directory: 'reference' },
+        // },
+      ],
+    }),
+  ],
 });
