4bc9c2ff7a68786df184f2c3aa150b8870992ebb
Milad Vafaeifard <milad.vafaeifard@gmail.com>
2025-03-29 00:03:32 +0100

    feat: add custom footer component to the configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -48,6 +48,7 @@ export default defineConfig({
       ],
       components: {
         PageTitle: './src/components/PageTitle.astro',
+        Footer: './src/components/overrides/Footer.astro',
       },
       head: [
         {
